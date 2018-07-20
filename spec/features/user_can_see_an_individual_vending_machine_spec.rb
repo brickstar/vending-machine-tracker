3d844bd85a9de94ccca4f176ece570c0cf6fb684
brickstar <brickstar@gmail.com>
2018-07-20 11:51:20 -0600

    formatted price to have 2 decimal places

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -4,7 +4,7 @@ feature 'When a user visits a vending machine show page' do
   scenario 'they see the location of that machine' do
     owner = Owner.create(name: "Sam's Snacks")
     dons  = owner.machines.create(location: "Don's Mixed Drinks")
-
+    snack = dons.snacks.create!(name: 'skittles', price: 1)
     visit machine_path(dons)
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
