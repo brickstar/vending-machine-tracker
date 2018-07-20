4a2719cc35431ba45a4d09c7cb1c6486735072f6
brickstar <brickstar@gmail.com>
2018-07-20 11:29:30 -0600

    average snack price method tested completed

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 describe Machine, type: :model do
   describe 'instance methods' do
     describe '.average_price_of_snacks' do
-      owner = Owner.create!(name: 'pearl')
-      machine = owner.machines.create!(location: 'turing')
-      snack1 = machine.snacks.create!(name: 'skittles', price: 1)
-      snack2 = machine.snacks.create!(name: 'sprees', price: 3)
+      it 'should return the average price of snacks in machine' do
+        owner = Owner.create!(name: 'pearl')
+        machine = owner.machines.create!(location: 'turing')
+        snack1 = machine.snacks.create!(name: 'skittles', price: 1)
+        snack2 = machine.snacks.create!(name: 'sprees', price: 3)
 
-      expect(machine.average_price_of_snacks).to eq(2)
+        expect(machine.average_price_of_snacks).to eq(2)
+      end
     end
   end
 end
