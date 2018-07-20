76d8c709a01c96dd2f3ce09b22a4f6ab2adf4e87
brickstar <brickstar@gmail.com>
2018-07-20 11:30:41 -0600

    user story 2 spec

--- spec/features/user_visits_vending_machine_spec.rb
+++ spec/features/user_visits_vending_machine_spec.rb
@@ -14,4 +14,15 @@ describe 'user visits machine show' do
     expect(page).to have_content(snack2.name)
     expect(page).to have_content(snack2.price)
   end
+
+  it 'should see average price of all snacks' do
+    owner = Owner.create!(name: 'pearl')
+    machine = owner.machines.create!(location: 'turing')
+    snack1 = machine.snacks.create!(name: 'skittles', price: 1)
+    snack2 = machine.snacks.create!(name: 'sprees', price: 3)
+
+    visit machine_path(machine)
+
+    expect(page).to have_content("Avg Price of Snacks: #{machine.average_price_of_snacks}")
+  end
 end
