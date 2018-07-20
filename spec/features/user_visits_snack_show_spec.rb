d8adf7c0304791c58e422ad7489ca0fbfb805c83
brickstar <brickstar@gmail.com>
2018-07-20 11:45:16 -0600

    machine locations spec and implemented in snack show view

--- spec/features/user_visits_snack_show_spec.rb
+++ spec/features/user_visits_snack_show_spec.rb
@@ -2,15 +2,29 @@ require 'rails_helper'
 
 describe 'user visits snack show page' do
   before(:each) do
-    @owner = Owner.create!(name: 'pearl')
-    @snack = Snack.create!(name: 'skittles', price: 1)
-    @machine = @snack.machines.create!(location: 'turing', owner: @owner)
+      @owner = Owner.create!(name: 'barry')
+      @snack1 = Snack.create!(price: 1, name: 'skittles')
+      @machine1 = @snack1.machines.create!(location: 'dons mixed drinks', owner: @owner)
+      @snack2 = @machine1.snacks.create!(price: 1, name: 'poprocks')
+      @machine2 = @snack1.machines.create!(location: 'turing basement', owner: @owner)
+      @snack3 = @machine2.snacks.create!(price: 3, name: 'sprees')
+      @snack4 = @machine2.snacks.create!(price: 3, name: 'lik a maid')
+      @snack4 = @machine2.snacks.create!(price: 3, name: 'tootsie pop')
   end
 
   it 'should see name and price of snack' do
-    visit snack_path(@snack)
+    visit snack_path(@snack1)
 
-    expect(page).to have_content(@snack.name)
-    expect(page).to have_content(@snack.price)
+    expect(page).to have_content(@snack1.name)
+    expect(page).to have_content(@snack1.price)
+  end
+
+  it 'should see list of locations of machines that carry it' do
+    visit snack_path(@snack1)
+
+    within('#locations') do
+      expect(page).to have_content(@machine1.location)
+      expect(page).to have_content(@machine2.location)
+    end
   end
 end
